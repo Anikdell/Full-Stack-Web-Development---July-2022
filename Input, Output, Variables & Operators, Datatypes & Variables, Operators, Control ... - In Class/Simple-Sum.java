e0c14d7014037418d745e27b223513f714ab69b5
Anik mitra <84117312+Anikdell@users.noreply.github.com>
2022-07-28 00:18:31 +0530

    Create Simple-Sum.java

---
+++ Input, Output, Variables & Operators, Datatypes & Variables, Operators, Control ... - In Class/Simple-Sum.java
@@ -0,0 +1,19 @@
+/* Author = ANIK MITRA */
+/* Language = JAVA */
+/* Address = Kolkata,WB,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		System.out.println((a+b+c));
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
