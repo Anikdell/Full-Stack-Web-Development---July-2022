3a792fa8132bbdb4b5601cb25abe72c2cc54e6ef
Anik mitra <84117312+Anikdell@users.noreply.github.com>
2022-07-28 00:15:47 +0530

    Create Nobita-and-profit.java

---
+++ Input, Output, Variables & Operators, Datatypes & Variables, Operators, Control ... - In Class/Nobita-and-profit.java
@@ -0,0 +1,24 @@
+/* Author = ANIK MITRA */
+/* Language = JAVA */
+/* Address = Kolkata,WB,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
+		int s=sc.nextInt();
+        int nobita=0;
+		if(c>s){
+            nobita=c-s;
+        }else{
+            nobita=s-c;
+        }
+		System.out.println(nobita);
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
