c0933f30578577bdb045fedd96b1a78cd0e368e3
Anik mitra <84117312+Anikdell@users.noreply.github.com>
2022-07-28 00:16:26 +0530

    Create Nobitss-test.java

---
+++ Input, Output, Variables & Operators, Datatypes & Variables, Operators, Control ... - In Class/Nobitss-test.java
@@ -0,0 +1,22 @@
+/* Author = ANIK MITRA */
+/* Language = JAVA */
+/* Address = Kolkata,WB,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int d =sc.nextInt();
+        int q =sc.nextInt();
+        if(q==0){
+         System.out.println("-1");
+        }else{
+         System.out.println(d/q);
+        }
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
