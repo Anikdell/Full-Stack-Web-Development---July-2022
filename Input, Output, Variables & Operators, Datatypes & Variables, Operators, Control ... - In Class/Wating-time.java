c8795fc5812b4da1fc7b20e5588b76c5161d2aa2
Anik mitra <84117312+Anikdell@users.noreply.github.com>
2022-07-28 00:19:51 +0530

    Create Wating-time.java

---
+++ Input, Output, Variables & Operators, Datatypes & Variables, Operators, Control ... - In Class/Wating-time.java
@@ -0,0 +1,19 @@
+/* Author = ANIK MITRA */
+/* Language = JAVA */
+/* Address = Kolkata,WB,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
+		int s=sc.nextInt();
+		// int nobita=s-c;
+		System.out.println((c*s));
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
